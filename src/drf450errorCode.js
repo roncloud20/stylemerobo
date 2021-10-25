3ad96f0ae67f4fd89f4a2ff96747de45418c67d0
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2021-10-26 00:07:07 +0100

    Adding the robofriends files to style me

---
+++ src/drf450errorCode.js
@@ -0,0 +1,26 @@
+export const errorCode = [
+	{
+		code : 1,
+		description: `Communication error with attachment control unit (D791-1).`,
+		limitation: `Attachment functions not working.`,
+		action: `Use diagnostic menu to find incorrect segment.`,
+		conCom: `D791-1`,
+		diagnostic : `CAN/ OWER,menu 2`,
+	},
+	{
+		code : 1,
+		description: `Communication error with attachment control unit (D791-1).`,
+		limitation: `Attachment functions not working.`,
+		action: `Use diagnostic menu to find incorrect segment.`,
+		conCom: `D791-1`,
+		diagnostic : `CAN/ OWER,menu 2`,
+	},
+	{
+		code : 1,
+		description: `Communication error with attachment control unit (D791-1).`,
+		limitation: `Attachment functions not working.`,
+		action: `Use diagnostic menu to find incorrect segment.`,
+		conCom: `D791-1`,
+		diagnostic : `CAN/ OWER,menu 2`,
+	},
+];
